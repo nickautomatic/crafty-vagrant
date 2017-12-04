0720db2ad93f0558bbbc9d4789406329f1358475
Nick Fraenkel <nickfraenkel@gmail.com>
2017-12-04 23:09:26 +0000

    Update main.js ES2015 example to fix linting error
    Previously this example code set a variable that wasn't used, which caused a linting error. To avoid this, we can just remove the variable declaration.
    
    While doing this, I've also added a little bit more clarification about what Babel does.

--- app/src/scripts/main.js
+++ app/src/scripts/main.js
@@ -1,12 +1,17 @@
 'use strict';
 
 // Import Javascript modules here:
-var example = require('./modules/example');
+const example = require('./modules/example');
 
 // ...and then do something with them:
 example.init();
 
 // This all gets processed by Babel, so feel free
 // to use ES2015 syntax if you like - it will be
-// transpiled to backwards-compatible ES5:
-let b = [1,2,3].map(x => x * 2);
+// transpiled to backwards-compatible ES5
+//
+// eg:
+[1,2,3].map(x => x * 2);
+
+// ...will be transpiled to:
+[1,2,3].map(function(x) { return x * 2; })
